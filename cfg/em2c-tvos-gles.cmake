6f76d2a22b1b2c9d437fddeab7227e9d0b0f4565
okuoku <mjt@cltn.org>
2021-07-12 01:49:36 +0900

    configs for Apple devices

---
+++ cfg/em2c-tvos-gles.cmake
@@ -0,0 +1,14 @@
+find_program(WASM2C wasm2c)
+
+execute_process(
+    COMMAND
+    ${CMAKE_COMMAND}
+    -G Xcode
+    -DCMAKE_SYSTEM_NAME=tvOS
+    -DYFRM_CWGL_USE_GLES=ON
+    -DEM2C_APPNAME=app1
+    -DWITH_WASM2C=${WASM2C}
+    ${CMAKE_CURRENT_LIST_DIR}/..
+    )
+
+message(STATUS "WASM2C = ${WASM2C}")
