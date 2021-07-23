3f89ea3111ee88029d4a9b80cfb8063ade75a4d7
okuoku <mjt@cltn.org>
2021-07-24 05:48:16 +0900

    Add tracker configurations

---
+++ cfg/em2c-win64-tracker-pvr.cmake
@@ -0,0 +1,13 @@
+execute_process(
+    COMMAND
+    ${CMAKE_COMMAND}
+    -G "Visual Studio 16 2019"
+    -A x64
+    -DYFRM_CWGL_USE_ANGLE=OFF
+    -DYFRM_CWGL_USE_GLES=ON
+    -DYFRM_CWGL_USE_PVRVFRAME=ON
+    -DYFRM_CWGL_EXPERIMENTAL_TRACKER=ON
+    -DEM2C_APPNAME=app1
+    -DWITH_WASM2C=${CMAKE_CURRENT_LIST_DIR}/../app/prepare/wabt-static/wasm2c.exe
+    ${CMAKE_CURRENT_LIST_DIR}/..
+    )
