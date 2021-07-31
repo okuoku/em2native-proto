7cf93065a33d3a36b027fe484f371c3ee5442644
okuoku <mjt@cltn.org>
2021-08-01 04:32:56 +0900

    Remove old config files

--- cfg/yfrm-win64-angle-vulkan.cmake
+++
@@ -1,9 +0,0 @@
-execute_process(
-    COMMAND
-    ${CMAKE_COMMAND}
-    -G "Visual Studio 16 2019"
-    -A x64
-    -DYFRM_CWGL_USE_ANGLE=ON
-    -DYFRM_CWGL_USE_VULKAN=ON
-    ${CMAKE_CURRENT_LIST_DIR}/../nodejs
-    )
